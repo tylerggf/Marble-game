6c0e617438d9aac78ad6c134515cc03d83139722
tylerggf <woodrobinson12345@gmail.com>
2026-06-09 20:57:09 -0700

    fix frontend hosting

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
     res.send("Marble Race Server Running!");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running");
 });
