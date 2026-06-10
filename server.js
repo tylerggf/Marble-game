60ae96e2f80df92a82fd5a6324f3b87846691b83
tylerggf <162388664+tylerggf@users.noreply.github.com>
2026-06-09 20:56:41 -0700

    Update server log message to include 'yay'

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log("Server running yay");
 });
